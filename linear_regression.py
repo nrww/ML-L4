e2474dad9eea42884c979a11676779c01fc03d19
nrww <narywow@gmail.com>
2024-03-27 20:10:33 +0500

    init

---
+++ linear_regression.py
@@ -0,0 +1,117 @@
+from __future__ import annotations
+
+from typing import List
+
+import numpy as np
+
+from descents import BaseDescent
+from descents import get_descent
+
+
+class LinearRegression:
+    """
+    Класс линейной регрессии.
+
+    Parameters
+    ----------
+    descent_config : dict
+        Конфигурация градиентного спуска.
+    tolerance : float, optional
+        Критерий остановки для квадрата евклидова нормы разности весов. По умолчанию равен 1e-4.
+    max_iter : int, optional
+        Критерий остановки по количеству итераций. По умолчанию равен 300.
+
+    Attributes
+    ----------
+    descent : BaseDescent
+        Экземпляр класса, реализующего градиентный спуск.
+    tolerance : float
+        Критерий остановки для квадрата евклидова нормы разности весов.
+    max_iter : int
+        Критерий остановки по количеству итераций.
+    loss_history : List[float]
+        История значений функции потерь на каждой итерации.
+
+    """
+
+    def __init__(self, descent_config: dict, tolerance: float = 1e-4, max_iter: int = 300, verbose: bool = False):
+        """
+        :param descent_config: gradient descent config
+        :param tolerance: stopping criterion for square of euclidean norm of weight difference (float)
+        :param max_iter: stopping criterion for iterations (int)
+        """
+        self.descent: BaseDescent = get_descent(descent_config)
+
+        self.tolerance: float = tolerance
+        self.max_iter: int = max_iter
+        self.verbose : bool = verbose
+        self.loss_history: List[float] = []
+        self.iteration : int = 0
+
+    def fit(self, x: np.ndarray, y: np.ndarray) -> LinearRegression:
+        """
+        Обучение модели линейной регрессии, подбор весов для наборов данных x и y.
+
+        Parameters
+        ----------
+        x : np.ndarray
+            Массив признаков.
+        y : np.ndarray
+            Массив целевых переменных.
+
+        Returns
+        -------
+        self : LinearRegression
+            Возвращает экземпляр класса с обученными весами.
+
+        """
+        self.loss_history.append(self.calc_loss(x, y))
+        for epoch in range(self.max_iter):
+            delta_w = self.descent.step(x, y)
+            loss = self.calc_loss(x, y)
+            self.loss_history.append(loss)
+            
+            if np.isnan(self.descent.w).any():
+                self.iteration = epoch + 1 
+                return self
+            if np.linalg.norm(delta_w) < self.tolerance:
+                self.iteration = epoch + 1 
+                return self           
+            if self.verbose and not epoch%25:
+                print(f"Epoch {epoch}\t Loss: {loss}")
+        self.iteration = self.max_iter
+        return self 
+
+    def predict(self, x: np.ndarray) -> np.ndarray:
+        """
+        Прогнозирование целевых переменных для набора данных x.
+
+        Parameters
+        ----------
+        x : np.ndarray
+            Массив признаков.
+
+        Returns
+        -------
+        prediction : np.ndarray
+            Массив прогнозируемых значений.
+        """
+        return self.descent.predict(x)
+
+    def calc_loss(self, x: np.ndarray, y: np.ndarray) -> float:
+        """
+        Расчёт значения функции потерь для наборов данных x и y.
+
+        Parameters
+        ----------
+        x : np.ndarray
+            Массив признаков.
+        y : np.ndarray
+            Массив целевых переменных.
+
+        Returns
+        -------
+        loss : float
+            Значение функции потерь.
+        """
+        return self.descent.calc_loss(x, y)
